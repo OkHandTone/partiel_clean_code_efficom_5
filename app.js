8427cc91f73940ffe121aad002618e0cd26052a4
MartinLeroy <erreurctos@gmail.com>
2026-04-03 14:29:37 +0200

    mise en place de la simmulation du code dans le app.js

--- app.js
+++ app.js
@@ -33,3 +33,21 @@ class NotificationInterne {
     console.log("notif interne");
   }
 }
+
+const email = new NotificationEmail();
+const sms = new NotificationSMS();
+const interne = new NotificationInterne();
+
+const NvCommande = new GestionnaireEvenement("nv_commande");
+const ExpeColis = new GestionnaireEvenement("ex_colis");
+
+NvCommande.ajouterAbonne(email);
+NvCommande.ajouterAbonne(interne);
+
+ExpeColis.ajouterAbonne(sms);
+
+console.log("client passe commande");
+NvCommande.declencher({ idCommande: "nv_commande", montant: 1500 });
+
+console.log("préparateur valide l'expédition...");
+ExpeColis.declencher({ idCommande: "expedition_nv_commande" });
